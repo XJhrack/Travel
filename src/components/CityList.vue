a6ee4832ee4ca64d1027e5b9ccb0389a7ba058e2
XJhrack <XJhrack@poke8.com>
2018-12-24 12:16:55 +0800

    city-list

---
+++ src/components/CityList.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div class="area">
+            <div class="title">热门城市</div>
+            <div class="button-list">
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title">A</div>
+            <div class="button-list">
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+                <div class="button">珠海</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+  name: 'CityList',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .area
+        overflow hidden
+        background #f5f5f5
+        .title
+            line-height .44rem
+            padding .2rem
+            color #212121
+        .button-list
+            overflow hidden
+            padding-right .35rem
+            .button
+                display block
+                background #fff
+                color #212121
+                width 32.5%
+                height .88rem
+                line-height .88rem
+                text-align center
+                float left
+                margin-left .06rem
+                margin-top .06rem
+
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -2,17 +2,20 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
 import CityHeader from '@/components/CityHeader.vue'
 import CitySearch from '@/components/CitySearch.vue'
+import CityList from '@/components/CityList.vue'
 export default {
   name: 'City',
   components:{
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   },
   data () {
     return {
