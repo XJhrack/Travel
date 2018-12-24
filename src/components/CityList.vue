71dc6c309f6ded7e088252f94229ae6f4cfae879
XJhrack <XJhrack@poke8.com>
2018-12-24 14:07:28 +0800

    city-list

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <div class="area">
+    <div class="list">
+        <div>
+            <div class="area">
             <div class="title">热门城市</div>
             <div class="button-list">
                 <div class="button">珠海</div>
@@ -10,22 +11,68 @@
                 <div class="button">珠海</div>
                 <div class="button">珠海</div>
             </div>
-        </div>
-        <div class="area">
-            <div class="title">A</div>
-            <div class="button-list">
-                <div class="button">珠海</div>
-                <div class="button">珠海</div>
-                <div class="button">珠海</div>
-                <div class="button">珠海</div>
-                <div class="button">珠海</div>
-                <div class="button">珠海</div>
+            </div>
+            <div class="area">
+                <div class="title">A</div>
+                <div class="button-list">
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title">A</div>
+                <div class="button-list">
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title">A</div>
+                <div class="button-list">
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title">A</div>
+                <div class="button-list">
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title">A</div>
+                <div class="button-list">
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                    <div class="button">珠海</div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
 
   name: 'CityList',
@@ -34,31 +81,41 @@ export default {
     return {
 
     }
+  },
+  mounted:function(){
+      const scroll = new BScroll('.list')
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-    .area
+    .list
         overflow hidden
+        position absolute
+        top 1.8rem
+        left 0
+        right 0
+        bottom 0
         background #f5f5f5
-        .title
-            line-height .44rem
-            padding .2rem
-            color #212121
-        .button-list
+        .area
             overflow hidden
-            padding-right .35rem
-            .button
-                display block
-                background #fff
+            .title
+                line-height .44rem
+                padding .2rem
                 color #212121
-                width 32.5%
-                height .88rem
-                line-height .88rem
-                text-align center
-                float left
-                margin-left .06rem
-                margin-top .06rem
+            .button-list
+                overflow hidden
+                padding-right .35rem
+                .button
+                    display block
+                    background #fff
+                    color #212121
+                    width 32.5%
+                    height .88rem
+                    line-height .88rem
+                    text-align center
+                    float left
+                    margin-left .06rem
+                    margin-top .06rem
 
 </style>
